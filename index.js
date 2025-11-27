92e20e0977676abcbd7ffb6f5f1fd90dcd4067d8
motionbaselab <admin@motionbaselab.com>
2025-11-28 01:39:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-const express = require("express");
-const crypto = require("crypto");
-const bodyParser = require("body-parser");
-const shopifyRoutes = require("./routes/shopify.js");
+import express from "express";
+import bodyParser from "body-parser";
+import shopifyRoutes from "./routes/shopify.js";
 
 const app = express();
 app.use(bodyParser.json());
 
-// 기본 API 체크
+// 기본 홈
 app.get("/", (req, res) => {
-  res.send("ZUNITY AUTO-LOOP Server Running OK!");
+    res.send("ZUNITY AUTO-LOOP Server Running OK!");
 });
 
-// Test route
-app.get("/test", (req, res) => {
-  res.send("TEST OK");
-});
-
-// Shopify Webhook 엔드포인트
+// Shopify 엔드포인트
 app.use("/api/shopify", shopifyRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
